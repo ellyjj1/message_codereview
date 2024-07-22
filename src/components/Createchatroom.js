3e9693fd18f6c08bda60d43f292ce59eb2610c43
ellyjj1 <72015673+ellyjj1@users.noreply.github.com>
2024-07-22 15:21:05 +1200

    add new page of creatchatroom, then users can create chatroom

---
+++ src/components/Createchatroom.js
@@ -0,0 +1,115 @@
+import React, {useEffect, useState} from 'react';
+import {BaseUrl} from "../consistents";
+import axios from "axios";
+
+function Createchatroom() {
+    const token = localStorage.getItem('authToken');
+    const login_user = localStorage.getItem('username');
+    const [local, setlocal] = useState(null)
+    const [users, setUsers] = useState([]);
+    const [create_status, setCreate_status] = useState("")
+
+    useEffect(() => {
+        let config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: BaseUrl + 'chat/users/',
+            headers: {
+                'Authorization': 'token dd542e46bc61deab8fc126c3114eda7cf0dcfdc4',
+                'Content-Type': 'application/json'
+            }
+        };
+
+        axios.request(config)
+            .then((response) => {
+                console.log(JSON.stringify(response.data));
+                setUsers(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+    }, );//change watching users to avoid loop
+
+    useEffect(() => {
+        let data = JSON.stringify({
+            "username": login_user
+        });
+
+        let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: BaseUrl + 'get_user_id/',
+            headers: {
+                'Authorization': 'token dd542e46bc61deab8fc126c3114eda7cf0dcfdc4',
+                'Content-Type': 'application/json'
+            },
+            data: data
+        };
+
+        axios.request(config)
+            .then((response) => {
+                console.log(JSON.stringify(response.data));//this is a dict
+                setlocal(response.data.user_id)//this is a int
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [login_user,token]);
+
+    function createChatRoom() {
+        let name = document.getElementById("name").value
+        let members = document.getElementById("members").selectedOptions
+        members = Array.from(members).map((member) => {
+            return Number.parseInt(member.value) || member.value
+        })
+
+        let data = JSON.stringify({
+            "name": name,
+            "created_by": local,
+            "members": members,
+
+        });
+
+        let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'https://message-peach-chi.vercel.app/chat/chatroom/',
+            headers: {
+                'Authorization': 'token dd542e46bc61deab8fc126c3114eda7cf0dcfdc4',
+                'Content-Type': 'application/json'
+            },
+            data: data
+        };
+
+        axios.request(config)
+            .then((response) => {
+                console.log(JSON.stringify(response.data));
+                setCreate_status("Create a New Chat Room Successful!")
+            })
+            .catch((error) => {
+                console.log(error);
+                setCreate_status("Some error happen, please login or try again late.")
+
+            });
+
+
+    }
+
+    return (
+        <div className="container mt-5">
+            <h1>Create Chat Room</h1>
+            <p>Name: <input id={"name"} type={"text"}/></p>
+            <p>Created By: {login_user}</p>
+            <p>Members: <select id="members" multiple="multiple">
+                {users.map((user) => {
+                    return <option key={user.id} value={user.id}>{user.username}</option>
+                })}
+            </select></p>
+            <button onClick={createChatRoom}>Create</button>
+            <p>{create_status}</p>
+        </div>
+    );
+}
+
+export default Createchatroom;
